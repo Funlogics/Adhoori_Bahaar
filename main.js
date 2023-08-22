db0ed4fafb0bea5eb7ff82cc043091a8ca3ead02
Funlogics <142270931+Funlogics@users.noreply.github.com>
2023-08-23 00:03:41 +0500

    root fixes

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ var home_Btn = document.getElementById("homeBtn");
 if(try_test){
 
     try_test.addEventListener("click", () => {     
-        try_test.href = "/choose.html";
+        try_test.href = "./choose.html";
      });
 }
 
 if(home_Btn){
     home_Btn.addEventListener('click',function(){ 
-        home_Btn.href = "/index.html"
+        home_Btn.href = "./index.html"
     });
 }
 
